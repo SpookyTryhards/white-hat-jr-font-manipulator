242de47910a77da4fa9a463425b200bf48ad2ed7
SpookyTryhards <94879186+SpookyTryhards@users.noreply.github.com>
2022-03-17 18:53:45 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,30 +1,44 @@
+leftWrist_x = 0;
+rightWrist_x = 0;
+difference = 0;
+
 function setup(){
-canvas = createCanvas(600, 600);
-canvas.position(560, 150);
-video = createCapture(VIDEO);
-video.size(600, 600);
-poseNet = ml5.poseNet(video,modelLoaded);
-poseNet.on('pose', gotPoses);
+    video = createCapture(VIDEO);
+    video.size(400,400);
+    video.position(10,50);
 
+    canvas = createCanvas(800,400);
+    canvas.position(430,130);
+
+    poseNet = ml5.poseNet(video,modelDone);
+    poseNet.on('pose',gotposes);
 }
-
-
 
 function draw(){
-background('#808080');
-
+    background("#0000FF");
+    document.getElementById("font_size").innerHTML = "Font Size Of The Text Will Be = "+difference+"px";
+    textSize(difference);
+    fill("#000000");
+    text('food',50,400);
 }
 
-function preload(){
-
+function modelDone(){
+    console.log("PoseNet Is Initialized And Loaded");
 }
 
-function modelLoaded(){
-    console.log(modelLoaded);
-}
+function gotposes(results,error){
+    if(error){
+        console.error(error);
+    }
+    if(results.length > 0){
+        console.log(results);
 
-function gotPoses(results){
-if(results.length > 0) {
-    console.log(results);
-   }
+        leftWrist_x = results[0].pose.leftWrist.x;
+        rightWrist_x = results[0].pose.rightWrist.x;
+
+        difference = floor(leftWrist_x - rightWrist_x);
+
+        console.log("rightWrist_x = "+results[0].pose.rightWrist.x + " rightWrist_y = "+results[0].pose.rightWrist.y);
+        console.log("leftWrist_x = "+results[0].pose.leftWrist.x + " leftWrist_y = "+results[0].pose.leftWrist.y);
+    }
 }
